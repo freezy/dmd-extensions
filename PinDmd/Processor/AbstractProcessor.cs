82b365ae88672e5072cd7ada61d1ca089050fca4
freezy <freezy@xbmc.org>
2016-04-13 17:55:50 +0200

    Added missing abstract class.

---
+++ PinDmd/Processor/AbstractProcessor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Reactive.Subjects;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace PinDmd.Processor
+{
+	public abstract class AbstractProcessor
+	{
+		public IObservable<BitmapSource> WhenProcessed => _whenProcessed;
+		protected Subject<BitmapSource> _whenProcessed = new Subject<BitmapSource>();
+
+		public bool Enabled { get; set; }
+		public abstract BitmapSource Process(BitmapSource bmp);
+
+	}
+}
