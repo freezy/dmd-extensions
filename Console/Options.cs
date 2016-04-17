6d897f677ca6129436449d5fe0570dbeaf800dee
freezy <freezy@xbmc.org>
2016-04-18 00:52:46 +0200

    Added skeleton for console app.

---
+++ Console/Options.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+using CommandLine.Text;
+
+namespace Console
+{
+	class Options
+	{
+		[Option('s', "source", HelpText = "The source you want to retrieve DMD data from.")]
+		public SourceType Source { get; set; }
+
+		[ParserState]
+		public IParserState LastParserState { get; set; }
+
+		[HelpOption]
+		public string GetUsage()
+		{
+			var help = new HelpText {
+				Heading = new HeadingInfo("dmdext", "1.0"),
+				Copyright = new CopyrightInfo("freezy", 2015),
+				AdditionalNewLineAfterOption = true,
+				AddDashesToOption = true
+			};
+			help.AddPreOptionsLine("Usage: dmdext --source=<source>");
+			help.AddOptions(this);
+
+
+			if (LastParserState.Errors.Any()) {
+				var errors = help.RenderParsingErrorsText(this, 2); // indent with two spaces
+
+				if (!string.IsNullOrEmpty(errors)) {
+					help.AddPreOptionsLine(string.Concat(Environment.NewLine, "ERROR(S):"));
+					help.AddPreOptionsLine(errors);
+				}
+			}
+			return help;
+		}
+
+	}
+
+	enum SourceType
+	{
+		PinballFX2,
+		Screen
+	}
+}
---
+++ Console/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var options = new Options();
+			if (CommandLine.Parser.Default.ParseArguments(args, options))
+			{
+
+				System.Console.WriteLine("Options okay!");
+			}
+		}
+	}
+}
