9faf2fc61d77b826f8ba809f1469c1bbe220bdc2
freezy <freezy@xbmc.org>
2016-05-08 23:17:31 +0200

    Added resize modes to transformation processor.

--- LibDmd/Processor/TransformationProcessor.cs
+++ LibDmd/Processor/TransformationProcessor.cs
@@ -1,4 +1,6 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using LibDmd.Output;
@@ -30,6 +32,11 @@ namespace LibDmd.Processor
 		/// </summary>
 		public bool FlipHorizontally { get; set; }
 
+		/// <summary>
+		/// How the image is resized
+		/// </summary>
+		public ResizeMode Resize { get; set; } = ResizeMode.Fit;
+
 		public override BitmapSource Process(BitmapSource bmp, IFrameDestination dest)
 		{
 			if (bmp.PixelWidth == Width && bmp.PixelHeight == Height && !FlipHorizontally && !FlipVertically) {
@@ -37,16 +44,106 @@ namespace LibDmd.Processor
 			}
 			var sw = new Stopwatch();
 			sw.Start();
-			var resized = new TransformedBitmap(bmp, new ScaleTransform(
-				Width / bmp.Width * (FlipHorizontally ? -1 : 1), 
-				Height / bmp.Height * (FlipVertically ? -1 : 1), 
-				(double)bmp.PixelWidth / 2, 
-				(double)bmp.PixelHeight / 2));
-			var rBmp = BitmapFrame.Create(resized);
-			rBmp.Freeze();
 
-			_whenProcessed.OnNext(rBmp);
-			return rBmp;
+			var srcAr = (double)bmp.PixelWidth / bmp.PixelHeight;
+			var destAr = (double)Width / Height;
+
+			double width;
+			double height;
+			var crop = false;
+			var fit = false;
+
+			const double tolerance = 0.01;
+
+			// resize to fill
+			if (Resize == ResizeMode.Fill && Math.Abs(destAr - srcAr) > tolerance) {
+				if (destAr > srcAr) {
+					width = Width;
+					height = Width / srcAr;
+				} else {
+					width = Height * srcAr;
+					height = Height;
+				}
+				crop = true;
+
+			// resize to fit
+			} else if (Resize == ResizeMode.Fit && Math.Abs(destAr - srcAr) > tolerance) {
+				if (destAr > srcAr) {
+					width = Height * srcAr;
+					height = Height;
+					
+				} else {
+					width = Width;
+					height = Width / srcAr;
+				}
+				fit = true;
+
+			// otherwise, stretch.
+			} else {
+				width = Width;
+				height = Height;
+			}
+
+			BitmapSource processedBmp = new TransformedBitmap(bmp, new ScaleTransform(width / bmp.PixelWidth * (FlipHorizontally ? -1 : 1), height / bmp.PixelHeight * (FlipVertically ? -1 : 1), (double)bmp.PixelWidth / 2, (double)bmp.PixelHeight / 2));
+
+			// filled needs cropping
+			if (crop) {
+				var cropParams = new Int32Rect(0, 0, Width, Height);
+				if (destAr > srcAr) {
+					cropParams.X = 0;
+					cropParams.Y = (int)((height - Height) / 2);
+				} else {
+					cropParams.X = (int)((width - Width) / 2);
+					cropParams.Y = 0;
+				}
+				processedBmp = new CroppedBitmap(processedBmp, cropParams);
+			}
+
+			// fit needs painting on new canvas
+			if (fit) {
+				var bytesPerPixel = (PixelFormats.Bgr32.BitsPerPixel + 7) / 8;
+				var blockSize = bytesPerPixel * processedBmp.PixelWidth * processedBmp.PixelHeight;
+				var buffer = new byte[blockSize];
+				var stride = processedBmp.PixelWidth * bytesPerPixel;
+
+				// create new canvas
+				var emptyBmp = new WriteableBitmap(Width, Height, 96, 96, PixelFormats.Bgr32, bmp.Palette);
+
+				// copy resized bitmap to new canvas
+				var rect = new Int32Rect(0, 0, processedBmp.PixelWidth, processedBmp.PixelHeight);
+				processedBmp.CopyPixels(rect, buffer, stride, 0);
+				if (destAr > srcAr) {
+					rect.X = (Width - processedBmp.PixelWidth) / 2;
+					rect.Y = 0;
+				} else {
+					rect.X = 0;
+					rect.Y = (Height - processedBmp.PixelHeight) / 2;
+				}
+				emptyBmp.WritePixels(rect, buffer, stride, 0);
+				processedBmp = emptyBmp;
+			}
+
+			processedBmp.Freeze();
+			_whenProcessed.OnNext(processedBmp);
+			return processedBmp;
+		}
+
+		public enum ResizeMode
+		{
+			/// <summary>
+			/// Stretch to fit dimensions. Aspect ratio is not kept.
+			/// </summary>
+			Stretch,
+
+			/// <summary>
+			/// Smaller dimensions fits while larger dimension gets cropped.
+			/// </summary>
+			Fill,
+
+			/// <summary>
+			/// Larger dimensions fits and smaller dimension stays black.
+			/// </summary>
+			Fit
 		}
 	}
 }
