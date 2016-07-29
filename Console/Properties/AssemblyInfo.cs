72639eda744abc4f13aa3fa29c46c53557e3035e
freezy <freezy@kodi.tv>
2016-07-29 21:47:14 +0200

    Released v1.3.0-beta1.

--- Console/Properties/AssemblyInfo.cs
+++ Console/Properties/AssemblyInfo.cs
@@ -35,6 +35,6 @@ using CommandLine;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.4.1")]
-[assembly: AssemblyFileVersion("1.2.4.1")]
-[assembly: AssemblyInformationalVersionAttribute("1.2.5-SNAPSHOT")]
+[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: AssemblyInformationalVersionAttribute("1.3.0-beta1")]
