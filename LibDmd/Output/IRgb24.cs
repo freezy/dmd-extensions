351432740ba1bd2da14e03059206ae83e3b26b9d
freezy <freezy@kodi.tv>
2016-12-04 00:55:47 +0100

    Added missing interface.

---
+++ LibDmd/Output/IRgb24.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace LibDmd.Output
+{
+	/// <summary>
+	/// Output devices implementing this interface are able to
+	/// render RGB 24-bit frames.
+	/// </summary>
+	public interface IRgb24
+	{
+		/// <summary>
+		/// Sets the color with which a grayscale source is rendered on the RGB display.
+		/// </summary>
+		/// <param name="color">Rendered color</param>
+		void SetColor(Color color);
+	}
+}
