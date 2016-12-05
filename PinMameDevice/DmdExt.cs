b05dcf507ad815f2710a7d7d9e2d6f9b75535b7f
freezy <freezy@kodi.tv>
2016-12-05 17:50:20 +0100

    Imports cleanup.

--- PinMameDevice/DmdExt.cs
+++ PinMameDevice/DmdExt.cs
@@ -1,10 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
-using System.Windows.Documents;
 using System.Windows.Threading;
 using DmdExt.Common;
 using LibDmd;
