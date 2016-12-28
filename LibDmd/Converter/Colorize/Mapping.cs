c9c1bff300a53f70a701cb33bad503c9c56f15b5
freezy <freezy@kodi.tv>
2016-12-28 21:13:19 +0100

    Fixed build bitness.

--- LibDmd/Converter/Colorize/Mapping.cs
+++ LibDmd/Converter/Colorize/Mapping.cs
@@ -47,13 +47,13 @@ namespace LibDmd.Converter.Colorize
 		public Mapping(BinaryReader reader)
 		{
 			Checksum = reader.ReadUInt32BE();
-			//Logger.Trace("  [{1}] [palette] Read checksum as {0}", Checksum, reader.BaseStream.Position);
+			Logger.Trace("  [{1}] [palette] Read checksum as {0}", Checksum, reader.BaseStream.Position);
 			Mode = reader.ReadByte();
-			//Logger.Trace("  [{1}] [palette] Read mode as {0}", Mode, reader.BaseStream.Position);
+			Logger.Trace("  [{1}] [palette] Read mode as {0}", Mode, reader.BaseStream.Position);
 			PaletteIndex = reader.ReadUInt16BE();
-			//Logger.Trace("  [{1}] [palette] Read index as {0}", PaletteIndex, reader.BaseStream.Position);
+			Logger.Trace("  [{1}] [palette] Read index as {0}", PaletteIndex, reader.BaseStream.Position);
 			Duration = reader.ReadUInt32BE();
-			//Logger.Trace("  [{1}] [palette] Read duration as {0}", Duration, reader.BaseStream.Position);
+			Logger.Trace("  [{1}] [palette] Read duration as {0}", Duration, reader.BaseStream.Position);
 		}
 	}
 }
