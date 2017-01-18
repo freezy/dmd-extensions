20b1981812f624d5a2c34f02ce60a0ad89deb89d
freezy <freezy@kodi.tv>
2017-01-19 00:02:38 +0100

    Updated dimension status while streaming.

--- LibDmd/Output/Network/VpdbStream.cs
+++ LibDmd/Output/Network/VpdbStream.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Media;
+using Newtonsoft.Json.Linq;
 using NLog;
 using Quobject.SocketIoClientDotNet.Client;
 
@@ -11,16 +12,19 @@ namespace LibDmd.Output.Network
 {
 	public class VpdbStream : IGray2Destination, IColoredGray2Destination, IResizableDestination
 	{
-		public string Name { get; } = "VPDB Streamer";
+		public string Name { get; } = "VPDB Stream";
 		public bool IsAvailable { get; } = true;
 
 		public string ApiKey { get; set; }
-		public string EndPoint { get; set; } = "http://127.0.0.1:3000/";
+		public string EndPoint { get; set; } = "https://api-test.vpdb.io/";
+		//public string EndPoint { get; set; } = "http://127.0.0.1:3000/";
 		public string AuthUser { get; set; }
 		public string AuthPass { get; set; }
 
 		private Socket _socket;
 		private bool _connected;
+		private int _width;
+		private int _height;
 
 		private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
 
@@ -31,7 +35,12 @@ namespace LibDmd.Output.Network
 			_socket.On(Socket.EVENT_CONNECT, () => {
 				_connected = true;
 				Logger.Info("Connected to VPDB.");
-				_socket.Emit("produce");
+				_socket.Emit("produce", new JObject { { "width", _width }, { "height", _height } });
+			});
+			_socket.On(Socket.EVENT_RECONNECT, () => {
+				_connected = true;
+				Logger.Info("Reconnected to VPDB.");
+				_socket.Emit("produce", new JObject { { "width", _width }, { "height", _height } });
 			});
 			_socket.On(Socket.EVENT_DISCONNECT, () => {
 				_connected = false;
@@ -45,15 +54,27 @@ namespace LibDmd.Output.Network
 
 		public void SetDimensions(int width, int height)
 		{
+			_width = width;
+			_height = height;
 			if (_connected) {
-				_socket.Emit("dimensions", new []{ width, height });
+				try {
+					_socket.Emit("dimensions", new JObject { { "width", width }, { "height", height } });
+				} catch (Exception e) {
+					Logger.Error(e, "Error sending frame to socket.");
+					_connected = false;
+				}
 			}
 		}
 
 		public void RenderGray2(byte[] frame)
 		{
 			if (_connected) {
-				_socket.Emit("gray2frame", frame);
+				try {
+					_socket.Emit("gray2frame", frame);
+				} catch (Exception e) {
+					Logger.Error(e, "Error sending frame to socket.");
+					_connected = false;
+				}
 			}
 		}
 
@@ -87,7 +108,7 @@ namespace LibDmd.Output.Network
 
 		public void Dispose()
 		{
-			_socket?.Emit("end");
+			_socket?.Emit("stop");
 			_socket?.Close();
 		}
 	}
