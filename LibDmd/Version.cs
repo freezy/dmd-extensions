7ee21422073a05ca997e6b5999710f75170bec2a
freezy <freezy@kodi.tv>
2018-06-21 01:04:10 +0200

    release: Beginning of v1.7.1.

--- LibDmd/Version.cs
+++ LibDmd/Version.cs
@@ -8,8 +8,8 @@ namespace LibDmd
 {
 	public class Version
 	{
-		public const string AssemblyVersion = "1.7.0.0";
-		public const string AssemblyFileVersion = "1.7.0.0";
-		public const string AssemblyInformationalVersionAttribute = "1.7.0";
+		public const string AssemblyVersion = "1.7.1.0";
+		public const string AssemblyFileVersion = "1.7.1.0";
+		public const string AssemblyInformationalVersionAttribute = "1.7.1";
 	}
 }
