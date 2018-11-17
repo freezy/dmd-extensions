12f8e1cbb104c99b3afb9a6cc43475928af19eb6
freezy <freezy@kodi.tv>
2018-11-18 00:28:30 +0100

    seg: Load SVG.

--- LibDmd/Output/Virtual/SegGenerator.cs
+++ LibDmd/Output/Virtual/SegGenerator.cs
@@ -2,30 +2,24 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using NLog;
 using SkiaSharp;
 
 namespace LibDmd.Output.Virtual
 {
-	public interface ISegGenerator
+	class SegGenerator
 	{
-		WriteableBitmap CreateImage(int width, int height);
-		void UpdateImage(WriteableBitmap writeableBitmap);
-	}
+		private readonly Assembly _assembly = Assembly.GetExecutingAssembly();
+		protected static readonly Logger Logger = LogManager.GetCurrentClassLogger();
 
-	class SegGenerator : ISegGenerator
-	{
-
-		private int Call = 0;
-		private Stopwatch Stopwatch = new Stopwatch();
-
-		public SegGenerator()
-		{
-		}
+		private int _call = 0;
+		private readonly Stopwatch _stopwatch = new Stopwatch();
 
 		public WriteableBitmap CreateImage(int width, int height)
 		{
@@ -46,17 +40,22 @@ namespace LibDmd.Output.Virtual
 				alphaType: SKAlphaType.Premul,
 				pixels: writeableBitmap.BackBuffer,
 				rowBytes: width * 4)) {
-				SKCanvas canvas = surface.Canvas;
+				var canvas = surface.Canvas;
 
-				int x = 30;
+				const int x = 30;
 				var paint = new SKPaint() { Color = new SKColor(255, 255, 255), TextSize = 100 };
+				var svg = new SKSvg();
+
+				svg.Load(_assembly.GetManifestResourceStream("LibDmd.Output.Virtual.alphanum.svg"));
+
 				canvas.Clear(new SKColor(130, 130, 130));
+				canvas.DrawPicture(svg.Picture);
 				canvas.DrawText("SkiaSharp on Wpf!", x, 200, paint);
-				if (this.Call == 0)
-					this.Stopwatch.Start();
-				double fps = this.Call / ((this.Stopwatch.Elapsed.TotalSeconds != 0) ? this.Stopwatch.Elapsed.TotalSeconds : 1);
+				if (this._call == 0)
+					this._stopwatch.Start();
+				double fps = _call / (_stopwatch.Elapsed.TotalSeconds != 0 ? _stopwatch.Elapsed.TotalSeconds : 1);
 				canvas.DrawText($"FPS: {fps:0}", x, 300, paint);
-				canvas.DrawText($"Frames: {this.Call++}", x, 400, paint);
+				canvas.DrawText($"Frames: {this._call++}", x, 400, paint);
 			}
 
 			writeableBitmap.AddDirtyRect(new Int32Rect(0, 0, width, height));
