a7025ff2eeaea737b5e67d8259698cf9ca9d92a9
freezy <freezy@kodi.tv>
2018-11-24 21:36:19 +0100

    fix: Bug in native convertion to uint16 array.

--- LibDmd/Common/InteropUtil.cs
+++ LibDmd/Common/InteropUtil.cs
@@ -12,11 +12,11 @@ namespace LibDmd.Common
 		public static ushort[] ReadUInt16Array(IntPtr data, int length)
 		{
 			var buffer = new ushort[length];
-			var byteBuffer = new byte[length * 2];
-			Marshal.Copy(data, byteBuffer, 0, length * 2);
+			var uint16Buffer = new byte[length * 2];
+			Marshal.Copy(data, uint16Buffer, 0, length * 2);
 			var pos = 0;
-			for (var i = 0; i < length; i += 2) {
-				buffer[pos++] = BitConverter.ToUInt16(byteBuffer, i);
+			for (var i = 0; i < length * 2; i += 2) {
+				buffer[pos++] = BitConverter.ToUInt16(uint16Buffer, i);
 			}
 			return buffer;
 		}
