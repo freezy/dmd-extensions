41fdf5759aa3515e5221d700e892506b5fbed2b1
freezy <freezy@kodi.tv>
2018-12-05 01:02:27 +0100

    fix: Only scale when necessary.

--- LibDmd/Output/Virtual/AlphaNumericResources.cs
+++ LibDmd/Output/Virtual/AlphaNumericResources.cs
@@ -319,20 +319,29 @@ namespace LibDmd.Output.Virtual
 		public int Display { get; set; }
 		public SegmentType SegmentType { get; set; }
 		public RasterizeDimensions Dim { get; set; }
-		public RasterizeStyle Style { get; set; } = new RasterizeStyle {
+		public RasterizeStyle Style {
+			get => _scaledStyle;
+			set { _style = value;
+				_scaledStyle = value.Scale(Dim);
+			}
+		}
+
+		public int NumLines { get; set; }
+		public int NumChars { get; set; }
+
+		private RasterizeStyle _style = new RasterizeStyle {
 			SkewAngle = -12,
 			Background = new RasterizeLayerStyle { Color = new SKColor(0xff, 0xff, 0xff, 0x20), Blur = new SKPoint(7, 7), IsEnabled = true, IsBlurEnabled = true, IsDilateEnabled = false },
 			OuterGlow = new RasterizeLayerStyle { Color = new SKColor(0xb6, 0x58, 0x29, 0x40), Blur = new SKPoint(50, 50), Dilate = new SKPoint(90, 40), IsEnabled = true, IsBlurEnabled = true, IsDilateEnabled = true },
 			InnerGlow = new RasterizeLayerStyle { Color = new SKColor(0xdd, 0x6a, 0x03, 0xa0), Blur = new SKPoint(15, 13), Dilate = new SKPoint(15, 10), IsEnabled = true, IsBlurEnabled = true, IsDilateEnabled = true },
 			Foreground = new RasterizeLayerStyle { Color = new SKColor(0xfb, 0xe6, 0xcb, 0xff), Blur = new SKPoint(2, 2), IsEnabled = true, IsBlurEnabled = true, IsDilateEnabled = false },
 		};
-		public int NumLines { get; set; }
-		public int NumChars { get; set; }
+		private RasterizeStyle _scaledStyle;
 
 		public void OnSvgsLoaded(SKRect svgSize, int canvasWidth, int canvasHeight)
 		{
-			Dim = new RasterizeDimensions(svgSize, canvasWidth, canvasHeight, NumChars, NumLines, Style.SkewAngle);
-			Style = Style.Scale(Dim);
+			Dim = new RasterizeDimensions(svgSize, canvasWidth, canvasHeight, NumChars, NumLines, _style.SkewAngle);
+			_scaledStyle = _style.Scale(Dim);
 		}
 	}
 
