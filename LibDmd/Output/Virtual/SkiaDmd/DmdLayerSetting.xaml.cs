79ee56d88e7e195c32ae6f0761ba0938c990d4f0
freezy <freezy@kodi.tv>
2019-01-01 01:24:58 +0100

    skia: Add dmd layer setting control and structure.

---
+++ LibDmd/Output/Virtual/SkiaDmd/DmdLayerSetting.xaml.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Reactive.Subjects;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using LibDmd.Output.Virtual.AlphaNumeric;
+using SkiaSharp;
+using SkiaSharp.Views.WPF;
+
+namespace LibDmd.Output.Virtual.SkiaDmd
+{
+	/// <summary>
+	/// Interaction logic for DmdLayerSetting.xaml
+	/// </summary>
+	public partial class DmdLayerSetting : UserControl
+	{
+		public ISubject<DmdLayerStyleDefinition> OnLayerChanged { get; } = new Subject<DmdLayerStyleDefinition>();
+
+		public DmdLayerStyleDefinition RasterizeStyleDefinition
+		{
+			get => _dmdLayerStyleDefinition;
+			set {
+				_dmdLayerStyleDefinition = value;
+				UpdateControls();
+			}
+		}
+
+		public string Label
+		{
+			get => LayerEnabled.Content.ToString();
+			set => LayerEnabled.Content = value;
+		}
+
+		private DmdLayerStyleDefinition _dmdLayerStyleDefinition;
+		private Color _color;
+
+		public DmdLayerSetting()
+		{
+			InitializeComponent();
+			LayerEnabled.Checked += (sender, e) => _dmdLayerStyleDefinition.IsEnabled = true;
+			LayerEnabled.Unchecked += (sender, e) => _dmdLayerStyleDefinition.IsEnabled = false;
+
+			DotSizeSlider.ValueChanged += (sender, e) => DotSizeValue.Text = DoubleToString(DotSizeSlider.Value);
+			DotSizeValue.TextChanged += (sender, e) => DotSizeSlider.Value = StringToDouble(DotSizeValue.Text, DotSizeSlider.Value);
+
+			OpacitySlider.ValueChanged += (sender, e) => OpacityValue.Text = DoubleToString(OpacitySlider.Value);
+			OpacityValue.TextChanged += (sender, e) => OpacitySlider.Value = StringToDouble(OpacityValue.Text, OpacitySlider.Value);
+
+			LuminositySlider.ValueChanged += (sender, e) => LuminosityValue.Text = DoubleToString(LuminositySlider.Value);
+			LuminosityValue.TextChanged += (sender, e) => LuminositySlider.Value = StringToDouble(LuminosityValue.Text, LuminositySlider.Value);
+
+			RoundedSlider.ValueChanged += (sender, e) => RoundedValue.Text = DoubleToString(RoundedSlider.Value);
+			RoundedValue.TextChanged += (sender, e) => RoundedSlider.Value = StringToDouble(RoundedValue.Text, RoundedSlider.Value);
+
+			BlurSlider.ValueChanged += (sender, e) => BlurValue.Text = DoubleToString(BlurSlider.Value);
+			BlurValue.TextChanged += (sender, e) => BlurSlider.Value = StringToDouble(BlurValue.Text, BlurSlider.Value);
+
+			LayerEnabled.Checked += (sender, e) => Changed();
+			LayerEnabled.Unchecked += (sender, e) => Changed();
+			RoundedEnabled.Checked += (sender, e) => Changed();
+			RoundedEnabled.Unchecked += (sender, e) => Changed();
+			BlurEnabled.Checked += (sender, e) => Changed();
+			BlurEnabled.Unchecked += (sender, e) => Changed();
+
+			DotSizeSlider.ValueChanged += (sender, e) => Changed();
+			OpacitySlider.ValueChanged += (sender, e) => Changed();
+			LuminositySlider.ValueChanged += (sender, e) => Changed();
+			RoundedSlider.ValueChanged += (sender, e) => Changed();
+			BlurSlider.ValueChanged += (sender, e) => Changed();
+		}
+
+		public void UpdateControls()
+		{
+			LayerEnabled.IsChecked = _dmdLayerStyleDefinition.IsEnabled;
+
+			DotSizeValue.Text = _dmdLayerStyleDefinition.Size.ToString();
+			DotSizeSlider.Value= _dmdLayerStyleDefinition.Size;
+			OpacityValue.Text = _dmdLayerStyleDefinition.Opacity.ToString();
+			OpacitySlider.Value= _dmdLayerStyleDefinition.Opacity;
+			LuminosityValue.Text = _dmdLayerStyleDefinition.Luminosity.ToString();
+			LuminositySlider.Value= _dmdLayerStyleDefinition.Luminosity;
+			RoundedEnabled.IsChecked = _dmdLayerStyleDefinition.IsRoundedEnabled;
+			RoundedValue.Text = _dmdLayerStyleDefinition.Rounded.ToString();
+			RoundedSlider.Value= _dmdLayerStyleDefinition.Rounded;
+			BlurEnabled.IsChecked = _dmdLayerStyleDefinition.IsBlurEnabled;
+			BlurValue.Text = _dmdLayerStyleDefinition.Blur.ToString();
+			BlurSlider.Value= _dmdLayerStyleDefinition.Blur;
+
+			ToggleRoundedEnabled(_dmdLayerStyleDefinition.IsRoundedEnabled);
+			ToggleBlurEnabled(_dmdLayerStyleDefinition.IsBlurEnabled);
+			ToggleLayerEnabled(_dmdLayerStyleDefinition.IsEnabled);
+		}
+
+		private void Changed()
+		{
+			if (LayerEnabled.IsChecked == null || BlurEnabled.IsChecked == null || RoundedEnabled.IsChecked == null) {
+				return;
+			}
+			OnLayerChanged.OnNext(new DmdLayerStyleDefinition {
+				IsEnabled = (bool)LayerEnabled.IsChecked,
+				Size = DotSizeSlider.Value,
+				Opacity = OpacitySlider.Value,
+				Luminosity = (float)LuminositySlider.Value,
+				IsRoundedEnabled = (bool)RoundedEnabled.IsChecked,
+				Rounded = RoundedSlider.Value,
+				IsBlurEnabled = (bool)BlurEnabled.IsChecked,
+				Blur = BlurSlider.Value
+			});
+		}
+
+		private void UpdateLayerEnabled(object sender, RoutedEventArgs e)
+		{
+			ToggleLayerEnabled(LayerEnabled.IsChecked != null && (bool) LayerEnabled.IsChecked);
+		}
+
+		private void ToggleLayerEnabled(bool enabled)
+		{
+			DotSizeSlider.IsEnabled = enabled;
+			DotSizeValue.IsEnabled = enabled;
+			OpacitySlider.IsEnabled = enabled;
+			OpacityValue.IsEnabled = enabled;
+			LuminositySlider.IsEnabled = enabled;
+			LuminosityValue.IsEnabled = enabled;
+			RoundedEnabled.IsEnabled = enabled;
+			RoundedSlider.IsEnabled = enabled;
+			RoundedValue.IsEnabled = enabled;
+			BlurEnabled.IsEnabled = enabled;
+			BlurSlider.IsEnabled = enabled;
+			BlurValue.IsEnabled = enabled;
+		}
+
+		private void UpdateBlurEnabled(object sender, RoutedEventArgs e)
+		{
+			ToggleBlurEnabled(BlurEnabled.IsChecked != null && (bool) BlurEnabled.IsChecked);
+		}
+
+		private void UpdateRoundedEnabled(object sender, RoutedEventArgs e)
+		{
+			ToggleRoundedEnabled(RoundedEnabled.IsChecked != null && (bool)RoundedEnabled.IsChecked);
+		}
+
+		private void ToggleBlurEnabled(bool enabled)
+		{
+			BlurSlider.IsEnabled = enabled;
+			BlurValue.IsEnabled = enabled;
+		}
+
+		private void ToggleRoundedEnabled(bool enabled)
+		{
+			RoundedSlider.IsEnabled = enabled;
+			RoundedValue.IsEnabled = enabled;
+		}
+
+		private static string DoubleToString(double d)
+		{
+			return ((int)Math.Round(d)).ToString();
+		}
+
+		private static double StringToDouble(string str, double fallback)
+		{
+			try {
+				return double.Parse(str);
+			} catch (Exception) {
+				return fallback;
+			}
+		}
+	}
+}
---
+++ LibDmd/Output/Virtual/SkiaDmd/DmdLayerStyle.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using LibDmd.Output.Virtual.AlphaNumeric;
+using SkiaSharp;
+
+namespace LibDmd.Output.Virtual.SkiaDmd
+{
+
+	public class DmdLayerStyleDefinition
+	{
+		/// <summary>
+		/// Defines if this layer is active
+		/// </summary>
+		public bool IsEnabled { get; set; }
+
+		/// <summary>
+		/// Defines if blurring of this layer is active
+		/// </summary>
+		public bool IsBlurEnabled { get; set; }
+
+		/// <summary>
+		/// Defines the dot is rounded
+		/// </summary>
+		public bool IsRoundedEnabled { get; set; }
+
+		/// <summary>
+		/// Dot size. 1 = 100%, but can be larger.
+		/// </summary>
+		public double Size { get; set; }
+
+		/// <summary>
+		/// Defines with which opacity the layer is drawn.
+		/// </summary>
+		public double Opacity { get; set; }
+
+		/// <summary>
+		/// Defines how much luminosity is added to the color. Can be negative.
+		/// </summary>
+		public float Luminosity { get; set; }
+
+		/// <summary>
+		/// How much the dots are rounded. 0 = square, 1 = circle.
+		/// </summary>
+		public double Rounded { get; set; }
+
+		/// <summary>
+		/// Defines how much blurring is applied to this layer
+		/// </summary>
+		public double Blur { get; set; }
+
+		/// <summary>
+		/// Returns a copy of this layer style with all parameters scaled by a given
+		/// factor.
+		/// </summary>
+		/// <param name="scaleFactor">Scale factor</param>
+		/// <returns></returns>
+		public DmdLayerStyle Scale(float scaleFactor)
+		{
+			return new DmdLayerStyle {
+				Size = scaleFactor * Size,
+				Opacity = Opacity,
+				Luminosity = Luminosity,
+				Rounded = Rounded,
+				Blur = scaleFactor * Blur
+			};
+		}
+
+		/// <summary>
+		/// Returns an identical copy of this layer style.
+		/// </summary>
+		/// <returns></returns>
+		public DmdLayerStyleDefinition Copy()
+		{
+			return new DmdLayerStyleDefinition {
+				IsEnabled = IsEnabled,
+				IsBlurEnabled = IsBlurEnabled,
+				IsRoundedEnabled = IsRoundedEnabled,
+				Size = Size,
+				Opacity = Opacity,
+				Luminosity = Luminosity,
+				Rounded = Rounded,
+				Blur = Blur
+			};
+		}
+
+		public override bool Equals(object obj)
+		{
+			if (!(obj is DmdLayerStyleDefinition item)) {
+				return false;
+			}
+			return IsEnabled == item.IsEnabled
+			       && IsBlurEnabled == item.IsBlurEnabled
+			       && IsRoundedEnabled == item.IsRoundedEnabled
+			       && Size.Equals(item.Size)
+			       && Opacity.Equals(item.Opacity)
+			       && Luminosity.Equals(item.Luminosity)
+			       && Rounded.Equals(item.Rounded)
+			       && Blur.Equals(item.Blur);
+		}
+
+		protected bool Equals(DmdLayerStyleDefinition other)
+		{
+			return IsEnabled == other.IsEnabled
+			       && IsBlurEnabled == other.IsBlurEnabled
+			       && IsRoundedEnabled == other.IsRoundedEnabled
+			       && Size.Equals(other.Size)
+			       && Opacity.Equals(other.Opacity)
+			       && Luminosity.Equals(other.Luminosity)
+			       && Rounded.Equals(other.Rounded)
+			       && Blur.Equals(other.Blur);
+		}
+
+		public int GetHashCode(DmdLayerStyleDefinition obj)
+		{
+			unchecked {
+				var hashCode = obj.IsEnabled.GetHashCode();
+				hashCode = (hashCode * 397) ^ obj.IsBlurEnabled.GetHashCode();
+				hashCode = (hashCode * 397) ^ obj.IsRoundedEnabled.GetHashCode();
+				hashCode = (hashCode * 397) ^ obj.Size.GetHashCode();
+				hashCode = (hashCode * 397) ^ obj.Opacity.GetHashCode();
+				hashCode = (hashCode * 397) ^ obj.Luminosity.GetHashCode();
+				hashCode = (hashCode * 397) ^ obj.Rounded.GetHashCode();
+				hashCode = (hashCode * 397) ^ obj.Blur.GetHashCode();
+				return hashCode;
+			}
+		}
+
+		public override string ToString()
+		{
+			return $"DmdLayerStyleDefinition[enabled:{IsEnabled},size:{Size},opacity:{Opacity},lum:{Luminosity},rounded:{IsRoundedEnabled}/{Rounded},blur:{IsBlurEnabled}/{Blur}]";
+		}
+	}
+
+	/// <summary>
+	/// Describes an individual layer of a given style.
+	/// </summary>
+	public class DmdLayerStyle
+	{
+		/// <summary>
+		/// Dot size. 1 = 100%, but can be larger.
+		/// </summary>
+		public double Size { get; set; }
+
+		/// <summary>
+		/// Defines with which opacity the layer is drawn.
+		/// </summary>
+		public double Opacity { get; set; }
+
+		/// <summary>
+		/// Defines how much luminosity is added to the color. Can be negative.
+		/// </summary>
+		public float Luminosity { get; set; }
+
+		/// <summary>
+		/// How much the dots are rounded. 0 = square, 1 = circle.
+		/// </summary>
+		public double Rounded { get; set; }
+
+		/// <summary>
+		/// Defines how much blurring is applied to this layer
+		/// </summary>
+		public double Blur { get; set; }
+
+		public override string ToString()
+		{
+			return $"DmdLayerStyle[size:{Size},opacity:{Opacity},rounded:{Rounded},blur:{Blur}]";
+		}
+	}
+}
