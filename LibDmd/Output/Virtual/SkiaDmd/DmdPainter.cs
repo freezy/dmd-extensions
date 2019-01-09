6332d7e1fa910dd351dcc794bf3d3ded80fee96e
freezy <freezy@kodi.tv>
2019-01-10 00:02:26 +0100

    skia: Fix opacity of f/x layers when pixel isn't solid.

--- LibDmd/Output/Virtual/SkiaDmd/DmdPainter.cs
+++ LibDmd/Output/Virtual/SkiaDmd/DmdPainter.cs
@@ -70,25 +70,84 @@ namespace LibDmd.Output.Virtual.SkiaDmd
 				Cache[layer] = cachedSurface;
 			}
 
-			Action<DmdData, DmdLayerStyleDefinition, Action<int, int, SKColor>> paint = null;
-			switch (data.Format) {
-				case FrameFormat.Gray2:
-					paint = PaintGray2;
-					break;
-				case FrameFormat.Gray4:
-					paint = PaintGray4;
-					break;
-				case FrameFormat.Rgb24:
-					paint = PaintRgb24;
-					break;
-			}
-			paint?.Invoke(data, styleDef, (x, y, color) => {
+			var painter = GetPainter(data.Format);
+			painter?.Invoke(data, styleDef, (x, y, color) => {
 				var dotPos = new SKPoint(x * blockSize.Width, y * blockSize.Height);
 				using (var dotPaint = new SKPaint()) {
 					dotPaint.ColorFilter = SKColorFilter.CreateBlendMode(GetColor(color, styleDef), SKBlendMode.SrcIn);
 					canvas.DrawSurface(cachedSurface, dotPos.X + (blockSize.Width - surfaceSize.Width) / 2, dotPos.Y + (blockSize.Height - surfaceSize.Height) / 2, dotPaint);
 				}
 			});
+		}
+
+		private static SKSurface PaintCache(DmdLayerStyleDefinition styleDef, SKSize blockSize, SKSize dotSize, SKSize surfaceSize)
+		{
+			var surface = GLUtil.GetInstance().CreateSurface((int)surfaceSize.Width, (int)surfaceSize.Height);
+			using (var dotPaint = new SKPaint()) {
+				dotPaint.IsAntialias = true;
+				dotPaint.Color = SKColors.Black;
+
+				if (styleDef.IsBlurEnabled) {
+					var blur = GetBlur(styleDef, blockSize);
+					dotPaint.ImageFilter = SKImageFilter.CreateBlur(blur, blur);
+				}
+				if (styleDef.IsRoundedEnabled) {
+					if (styleDef.Rounded < 1) {
+						var cornerRadius = Math.Min(dotSize.Width, dotSize.Height) * (float)styleDef.Rounded / 2;
+						surface.Canvas.DrawRoundRect(surfaceSize.Width / 2 - dotSize.Width / 2, surfaceSize.Height / 2 - dotSize.Width / 2, dotSize.Width, dotSize.Height, cornerRadius, cornerRadius, dotPaint);
+					} else {
+						var dotRadius = Math.Min(dotSize.Width, dotSize.Height) / 2;
+						surface.Canvas.DrawCircle(surfaceSize.Width / 2, surfaceSize.Height / 2, dotRadius, dotPaint);
+					}
+				} else {
+					surface.Canvas.DrawRect(surfaceSize.Width / 2 - dotSize.Width / 2, surfaceSize.Height / 2 - dotSize.Width / 2, dotSize.Width, dotSize.Height, dotPaint);
+				}
+			}
+			return surface;
+		}
+
+		private static void PaintDirectly(DmdData data, SKCanvas canvas, int width, int height, DmdLayerStyleDefinition styleDef)
+		{
+			var size = GetBlockSize(data, width, height);
+			var dotSize = new SKSize((float)styleDef.Size * width / data.Width, (float)styleDef.Size * height / data.Height);
+			var painter = GetPainter(data.Format);
+
+			painter?.Invoke(data, styleDef, (x, y, color) => {
+				using (var dotPaint = new SKPaint()) {
+					dotPaint.IsAntialias = true;
+					dotPaint.Color = GetColor(color, styleDef);
+
+					if (styleDef.IsBlurEnabled) {
+						var blur = GetBlur(styleDef, size);
+						dotPaint.ImageFilter = SKImageFilter.CreateBlur(blur, blur);
+					}
+					var dotPos = new SKPoint(x * size.Width, y * size.Height);
+					if (styleDef.IsRoundedEnabled) {
+						if (styleDef.Rounded < 1) {
+							var cornerRadius = Math.Min(dotSize.Width, dotSize.Height) * (float)styleDef.Rounded / 2;
+							canvas.DrawRoundRect(dotPos.X + size.Width / 2 - dotSize.Width / 2, dotPos.Y + size.Height / 2 - dotSize.Width / 2, dotSize.Width, dotSize.Height, cornerRadius, cornerRadius, dotPaint);
+						} else {
+							var dotRadius = Math.Min(dotSize.Width, dotSize.Height) / 2;
+							canvas.DrawCircle(dotPos.X + size.Width / 2, dotPos.Y + size.Height / 2, dotRadius, dotPaint);
+						}
+					} else {
+						canvas.DrawRect(dotPos.X + size.Width / 2 - dotSize.Width / 2, dotPos.Y + size.Height / 2 - dotSize.Width / 2, dotSize.Width, dotSize.Height, dotPaint);
+					}
+				}
+			});
+		}
+
+		private static Action<DmdData, DmdLayerStyleDefinition, Action<int, int, SKColor>> GetPainter(FrameFormat format)
+		{
+			switch (format) {
+				case FrameFormat.Gray2:
+					return PaintGray2;
+				case FrameFormat.Gray4:
+					return PaintGray4;
+				case FrameFormat.Rgb24:
+					return PaintRgb24;
+			}
+			return null;
 		}
 
 		private static void PaintGray2(DmdData data, DmdLayerStyleDefinition styleDef, Action<int, int, SKColor> paint)
@@ -122,72 +181,6 @@ namespace LibDmd.Output.Virtual.SkiaDmd
 					var framePos = y * data.Width * 3 + x;
 					if (styleDef.IsUnlit || data.Data[framePos] != 0 || data.Data[framePos + 1] != 0 || data.Data[framePos + 2] != 0) {
 						paint((int)(x / 3f), y, new SKColor(data.Data[framePos], data.Data[framePos + 1], data.Data[framePos + 2]));
-					}
-				}
-			}
-		}
-
-
-		private static SKSurface PaintCache(DmdLayerStyleDefinition styleDef, SKSize blockSize, SKSize dotSize, SKSize surfaceSize)
-		{
-			var surface = GLUtil.GetInstance().CreateSurface((int)surfaceSize.Width, (int)surfaceSize.Height);
-			using (var dotPaint = new SKPaint()) {
-				dotPaint.IsAntialias = true;
-				dotPaint.Color = SKColors.Black;
-
-				if (styleDef.IsBlurEnabled) {
-					var blur = GetBlur(styleDef, blockSize);
-					dotPaint.ImageFilter = SKImageFilter.CreateBlur(blur, blur);
-				}
-				if (styleDef.IsRoundedEnabled) {
-					if (styleDef.Rounded < 1) {
-						var cornerRadius = Math.Min(dotSize.Width, dotSize.Height) * (float)styleDef.Rounded / 2;
-						surface.Canvas.DrawRoundRect(surfaceSize.Width / 2 - dotSize.Width / 2, surfaceSize.Height / 2 - dotSize.Width / 2, dotSize.Width, dotSize.Height, cornerRadius, cornerRadius, dotPaint);
-					} else {
-						var dotRadius = Math.Min(dotSize.Width, dotSize.Height) / 2;
-						surface.Canvas.DrawCircle(surfaceSize.Width / 2, surfaceSize.Height / 2, dotRadius, dotPaint);
-					}
-				} else {
-					surface.Canvas.DrawRect(surfaceSize.Width / 2 - dotSize.Width / 2, surfaceSize.Height / 2 - dotSize.Width / 2, dotSize.Width, dotSize.Height, dotPaint);
-				}
-			}
-			return surface;
-		}
-
-		private static void PaintDirectly(DmdData data, SKCanvas canvas, int width, int height, DmdLayerStyleDefinition styleDef)
-		{
-			var size = GetBlockSize(data, width, height);
-			var dotSize = new SKSize((float)styleDef.Size * width / data.Width, (float)styleDef.Size * height / data.Height);
-			for (var y = 0; y < data.Height; y++) {
-				for (var x = 0; x < data.Width * 3; x += 3) {
-					var framePos = y * data.Width * 3 + x;
-
-					// don't render black dots at all if lit
-					if (!styleDef.IsUnlit && data.Data[framePos] == 0 && data.Data[framePos + 1] == 0 && data.Data[framePos + 2] == 0) {
-						continue;
-					}
-
-					var color = new SKColor(data.Data[framePos], data.Data[framePos + 1], data.Data[framePos + 2]);
-					using (var dotPaint = new SKPaint()) {
-						dotPaint.IsAntialias = true;
-						dotPaint.Color = GetColor(color, styleDef);
-
-						if (styleDef.IsBlurEnabled) {
-							var blur = GetBlur(styleDef, size);
-							dotPaint.ImageFilter = SKImageFilter.CreateBlur(blur, blur);
-						}
-						var dotPos = new SKPoint(x / 3f * size.Width, y * size.Height);
-						if (styleDef.IsRoundedEnabled) {
-							if (styleDef.Rounded < 1) {
-								var cornerRadius = Math.Min(dotSize.Width, dotSize.Height) * (float)styleDef.Rounded / 2;
-								canvas.DrawRoundRect(dotPos.X + size.Width / 2 - dotSize.Width / 2, dotPos.Y + size.Height / 2 - dotSize.Width / 2, dotSize.Width, dotSize.Height, cornerRadius, cornerRadius, dotPaint);
-							} else {
-								var dotRadius = Math.Min(dotSize.Width, dotSize.Height) / 2;
-								canvas.DrawCircle(dotPos.X + size.Width / 2, dotPos.Y + size.Height / 2, dotRadius, dotPaint);
-							}
-						} else {
-							canvas.DrawRect(dotPos.X + size.Width / 2 - dotSize.Width / 2, dotPos.Y + size.Height / 2 - dotSize.Width / 2, dotSize.Width, dotSize.Height, dotPaint);
-						}
 					}
 				}
 			}
@@ -227,7 +220,7 @@ namespace LibDmd.Output.Virtual.SkiaDmd
 				color = SKColor.FromHsl(h, s, Math.Max(0, Math.Min(100, l + styleDef.Luminosity)));
 			}
 			if (styleDef.Opacity < 1) {
-				color = color.WithAlpha((byte)(256 * styleDef.Opacity));
+				color = color.WithAlpha((byte)(color.Alpha * styleDef.Opacity));
 			}
 			return color;
 		}
