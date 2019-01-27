3f5a11c22eb828ee52b5401672859c8161ac0c3d
freezy <freezy@kodi.tv>
2019-01-28 00:12:33 +0100

    msi: Fix version string.

--- VersionAssemblyInfo.cs
+++ VersionAssemblyInfo.cs
@@ -15,5 +15,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.7.3.0")]
 [assembly: AssemblyFileVersion("1.7.3.0")]
-[assembly: AssemblyInformationalVersionAttribute("1.7.3.0")]
+[assembly: AssemblyInformationalVersionAttribute("1.7.3-SNAPSHOT")]
 [assembly: NeutralResourcesLanguage("en")]
