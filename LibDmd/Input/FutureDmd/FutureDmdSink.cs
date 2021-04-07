7b0bb78732bfa4e8a4f8a65691499cdd6cf5b785
freezy <freezy@kodi.tv>
2021-04-08 01:31:57 +0200

    fp: Add pipe loop.

--- LibDmd/Input/FutureDmd/FutureDmdSink.cs
+++ LibDmd/Input/FutureDmd/FutureDmdSink.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.IO.Pipes;
 using System.Linq;
 using System.Reactive;
@@ -22,7 +23,7 @@ namespace LibDmd.Input.FutureDmd
 		public IObservable<Unit> OnPause => _onPause;
 
 		private const string PipeName = "futuredmd"; 
-		private Thread _thread;
+		private readonly Thread _thread;
 
 		private readonly ISubject<Unit> _onResume = new Subject<Unit>();
 		private readonly ISubject<Unit> _onPause = new Subject<Unit>();
@@ -42,7 +43,6 @@ namespace LibDmd.Input.FutureDmd
 			Thread.Sleep(250);
 		}
 
-
 		public IObservable<DMDFrame> GetGray4Frames()
 		{
 			return _framesGray4;
@@ -50,22 +50,44 @@ namespace LibDmd.Input.FutureDmd
 
 		private void ServerThread(object data) 
 		{
-			var server = new NamedPipeServerStream(PipeName, PipeDirection.In, 1, PipeTransmissionMode.Message, PipeOptions.None);
+			try
+			{
+				var server = new NamedPipeServerStream(PipeName, PipeDirection.In, 1, PipeTransmissionMode.Message);
+				server.WaitForConnection();
 
-			// wait for future pinball to connect
-			server.WaitForConnection();
+				var isGameRunning = true;
+				var chunkSize = 0;
+				var gray2Frame = new DMDFrame { width = 132, height = 32 };
+				var messageChunk = new byte[4096];
+				
+				// for each frame
+				do
+				{
+					// for each chunk
+					do
+					{
+						chunkSize = server.Read(messageChunk, 0, messageChunk.Length);
+						if (chunkSize > 0) {
+							Logger.Info($"Got chunk ({chunkSize}):\n" + Encoding.UTF8.GetString(messageChunk.Take(chunkSize).ToArray()));
+						}
 
-			Logger.Info($"FutureDMD connected to pipe.");
+					} while (!server.IsMessageComplete);
 
-			var messageChunk = new byte[4096];
-			do {
-				var chunkSize = server.Read(messageChunk, 0, messageChunk.Length);
-				Logger.Info($"Got chunk ({chunkSize}):\n" + Encoding.UTF8.GetString(messageChunk));
+					// TODO convert message to frame data
+					var frame = messageChunk;
 
-			} while (!server.IsMessageComplete);
+					// publish frame data
+					_framesGray4.OnNext(gray2Frame.Update(frame));
 
 
-			Logger.Info($"Pipe server for FutureDMD terminated!");
+				} while (isGameRunning);
+
+				server.Disconnect();
+				Logger.Info($"Pipe server for FutureDMD terminated!");
+
+			} catch (IOException e) {
+				Logger.Error(e);
+			}
 		}
 	}
 }
