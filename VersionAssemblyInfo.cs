893c34b65fea4f3167c449ce060c24836cecddb8
freezy <freezy@kodi.tv>
2022-02-07 22:07:37 +0100

    release: v1.10.0.

--- VersionAssemblyInfo.cs
+++ VersionAssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.9.1.0")]
-[assembly: AssemblyFileVersion("1.9.1.0")]
-[assembly: AssemblyInformationalVersion("1.9.1-SNAPSHOT")]
+[assembly: AssemblyVersion("1.10.0.0")]
+[assembly: AssemblyFileVersion("1.10.0.0")]
+[assembly: AssemblyInformationalVersion("1.10.0")]
 [assembly: NeutralResourcesLanguage("en")]
