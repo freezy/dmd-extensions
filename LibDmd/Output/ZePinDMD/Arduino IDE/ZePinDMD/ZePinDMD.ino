c19801662b44effe075991f8277963c36d1b2d2e
David <okidoaki@hotmail.fr>
2022-04-09 12:37:54 +0200

    Modified to accept any resolution

--- LibDmd/Output/ZePinDMD/Arduino IDE/ZePinDMD/ZePinDMD.ino
+++ LibDmd/Output/ZePinDMD/Arduino IDE/ZePinDMD/ZePinDMD.ino
@@ -1,8 +1,13 @@
+#define PANEL_WIDTH 64
+#define PANEL_HEIGHT 32   
+#define PANELS_NUMBER 2   // Number of chained panels
+
 // ------------------------------------------ ZePinDMD by Zedrummer (http://pincabpassion.net)---------------------------------------------
 // - Install the ESP32 board in Arduino IDE as explained here https://randomnerdtutorials.com/installing-the-esp32-board-in-arduino-ide-windows-instructions/
 // - Install SPIFFS file system as explained here https://randomnerdtutorials.com/install-esp32-filesystem-uploader-arduino-ide/
 // - Install "ESP32 HUB75 LED MATRIX PANNEL DMA" Display library via the library manager
 // - Go to menu "Tools" then click on "ESP32 Sketch Data Upload"
+// - Change the values in the 3 first lines above (PANEL_WIDTH, PANEL_HEIGHT, PANELS_NUMBER) according the pannels
 // - Inject this code in the board
 // - If you have blurry pictures, the display is not clean, try to reduce the input voltage of your LED matrix pannels, often, 5V pannels need
 // between 4V and 4.5V to display clean pictures, you often have a screw in switch-mode power supplies to change the output voltage a little bit
@@ -11,12 +16,11 @@
 // until the display is correct (automatically saved, no need to do it again)
 // -----------------------------------------------------------------------------------------------------------------------------------------
 
+#define PANE_WIDTH PANEL_WIDTH * PANELS_NUMBER
+#define PANE_HEIGHT PANEL_HEIGHT
+
 #include <ESP32-HUB75-MatrixPanel-I2S-DMA.h>
 #include <SPIFFS.h>
-
-#define PANEL_WIDTH 64
-#define PANEL_HEIGHT 32   
-#define PANELS_NUMBER 2   // Number of chained panels
 
 /* Pinout from ESP32-HUB75-MatrixPanel-I2S-DMA.h
     #define R1_PIN_DEFAULT  25
@@ -37,12 +41,35 @@
     #define CLK_PIN_DEFAULT 16
  */
 
+// Change these to whatever suits
+#define R1_PIN 25
+#define G1_PIN 26
+#define B1_PIN 27
+#define R2_PIN 14
+#define G2_PIN 12
+#define B2_PIN 13
+#define A_PIN 23
+#define B_PIN 19
+#define C_PIN 5
+#define D_PIN 17
+#define E_PIN 22 // required for 1/32 scan panels, like 64x64. Any available pin would do, i.e. IO32
+#define LAT_PIN 4
+#define OE_PIN 15
+#define CLK_PIN 16
+
+HUB75_I2S_CFG::i2s_pins _pins={R1_PIN, G1_PIN, B1_PIN, R2_PIN, G2_PIN, B2_PIN, A_PIN, B_PIN, C_PIN, D_PIN, E_PIN, LAT_PIN, OE_PIN, CLK_PIN};
+HUB75_I2S_CFG mxconfig(
+          PANEL_WIDTH,   // width
+          PANEL_HEIGHT,   // height
+           PANELS_NUMBER,   // chain length
+         _pins//,   // pin mapping
+         //HUB75_I2S_CFG::FM6126A      // driver chip
+);
+MatrixPanel_I2S_DMA *dma_display = nullptr;
+
 int ordreRGB[3*6]={0,1,2, 2,0,1, 1,2,0,
                    0,2,1, 1,0,2, 2,1,0};
 int acordreRGB=0;
-
-#define PANE_WIDTH PANEL_WIDTH * PANELS_NUMBER
-#define PANE_HEIGHT PANEL_HEIGHT
 
 unsigned char Palette4[3*4]; // palettes 4 couleurs et 16 couleurs en RGB
 static const float levels4[4]  = {10,33,67,100};
@@ -50,9 +77,6 @@ unsigned char Palette16[3*16];
 static const float levels16[16]  = {0, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 90, 100}; // SAM brightness seems okay
 unsigned char Palette64[3*64];
 static const float levels64[64]  = {0, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 90, 100}; // SAM brightness seems okay
-
-// placeholder for the matrix object
-MatrixPanel_I2S_DMA *dma_display = nullptr;
 
 unsigned char pannel[PANE_WIDTH*PANE_HEIGHT*3];
 
@@ -99,25 +123,7 @@ void fillpannel()
   {
     for (int ti = 0; ti < PANE_WIDTH; ti++)
     {
-      
-      
-      
-      
-      
-      
-      
-      
-      
       dma_display->drawPixelRGB888(ti, tj, pannel[ti * 3 + tj * 3 * 128 + ordreRGB[acordreRGB * 3]], pannel[ti * 3 + tj * 3 * 128 + ordreRGB[acordreRGB * 3 + 1]], pannel[ti * 3 + tj * 3 * 128 + ordreRGB[acordreRGB * 3 + 2]]);
-
-
-
-
-
-
-
-
-      
     }
   }
 }
@@ -193,10 +199,6 @@ void setup()
 
   pinMode(ORDRE_BUTTON_PIN, INPUT_PULLUP);
     
-  HUB75_I2S_CFG mxconfig;
-  mxconfig.mx_height = PANEL_HEIGHT;      // we have 32 pix high panels
-  mxconfig.mx_width = PANEL_WIDTH;      // we have 64 pix wide panels
-  mxconfig.chain_length = PANELS_NUMBER;  // we have 2 pannels chained
   mxconfig.clkphase = false; // change if you have some parts of the pannel with a shift
   dma_display = new MatrixPanel_I2S_DMA(mxconfig);
   dma_display->begin();
@@ -276,12 +278,15 @@ void loop()
     while (!Serial.available());
     c4 = Serial.read();
   }
-  if (c4==15) // shake hands with the computer
+  if (c4 == 12) // ask for resolution (and shake hands)
   {
     Serial.write(0x81);
     Serial.write(0xC3);
     Serial.write(0xE7);
-    Serial.write(15);
+    Serial.write(PANE_WIDTH&0xff);
+    Serial.write((PANE_WIDTH>>8)&0xff);
+    Serial.write(PANE_HEIGHT&0xff);
+    Serial.write((PANE_HEIGHT>>8)&0xff);
   }
   else if (c4 == 6) // reinit palettes
   {
