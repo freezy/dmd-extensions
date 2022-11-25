f9bbefe44d9b77b5961f40cc8dff7329f5b57e90
Vincent Bousquet <vincent.bousqt@gmail.com>
2022-11-25 22:24:45 +0100

    Add support for dmddevicePUP64.dll

--- LibDmd/Output/PinUp/PinUpOutput.cs
+++ LibDmd/Output/PinUp/PinUpOutput.cs
@@ -39,7 +39,7 @@ namespace LibDmd.Output.PinUp
 
 			var localPath = new Uri(Assembly.GetExecutingAssembly().CodeBase).LocalPath;
 			var assemblyFolder = Path.GetDirectoryName(localPath);
-			var dllFileName = Path.Combine(assemblyFolder, "dmddevicePUP.DLL");
+			var dllFileName = Path.Combine(assemblyFolder, Environment.Is64BitProcess ? "dmddevicePUP64.DLL" : "dmddevicePUP.DLL");
 			var pDll = NativeMethods.LoadLibrary(dllFileName);
 
 			if (pDll == IntPtr.Zero) {
