6876fa3e6ddd7d09b0cffd42f937d24b52da3f96
David <okidoaki@hotmail.fr>
2022-12-10 17:52:07 +0100

    Updates for cRom and colour rotations

--- LibDmd/ColoredFrame.cs
+++ LibDmd/ColoredFrame.cs
@@ -20,17 +20,43 @@ namespace LibDmd
 		/// </summary>
 		public int PaletteIndex { get; }
 
+		/// <summary>
+		/// Rotation descriptions.
+		/// </summary>
+		public bool isRotation = false;
+		public byte[] Rotations { get; }
+
 		public ColoredFrame(byte[][] planes, Color[] palette, int paletteIndex)
 		{
 			Planes = planes;
 			Palette = palette;
 			PaletteIndex = paletteIndex;
+			isRotation = false;
+		}
+
+		public ColoredFrame(byte[][] planes, Color[] palette, int paletteIndex,bool isrotation, byte[] rotations)
+		{
+			Planes = planes;
+			Palette = palette;
+			PaletteIndex = paletteIndex;
+			isRotation = isrotation;
+			Rotations= rotations;
 		}
 
 		public ColoredFrame(byte[][] planes, Color[] palette)
 		{
 			Planes = planes;
 			Palette = palette;
+			isRotation= false;
+			PaletteIndex = -1;
+		}
+
+		public ColoredFrame(byte[][] planes, Color[] palette, byte[] rotations)
+		{
+			Planes = planes;
+			Palette = palette;
+			isRotation= true;
+			Rotations = rotations;
 			PaletteIndex = -1;
 		}
 
@@ -38,6 +64,7 @@ namespace LibDmd
 		{
 			Planes = FrameUtil.Split(width, height, 2, frame);
 			Palette = ColorUtil.GetPalette(new[] { Colors.Black, color }, 4);
+			isRotation = false;
 		}
 	}
 }
