84397c77505dd5fff57485669b5ed71779a93bed
David <okidoaki@hotmail.fr>
2022-12-10 17:53:36 +0100

    Updates for cRom and colour rotations

--- LibDmd/Converter/cRom/cRom.cs
+++ LibDmd/Converter/cRom/cRom.cs
@@ -1,20 +1,23 @@
 ﻿using System;
 using System.IO;
+using System.IO.Compression;
+using System.Linq;
 using System.Reactive;
 using System.Reactive.Subjects;
+using System.Security.Cryptography;
 using System.Windows.Media;
 using LibDmd.Common;
-using LibDmd.Converter.Colorize;
-//using LibDmd.Converter.Colorize;
 using LibDmd.Input;
-using Xceed.Wpf.Toolkit;
+using NLog;
+using SharpGL;
 
 namespace LibDmd.Converter.cRom
 {
 	public class cRom : AbstractSource, IConverter, IColoredGray6Source
 	{
-		public override string Name { get; } = "2-bit source to ColorizedRom";
+		public override string Name { get; } = "converter to ColorizedRom";
 		public FrameFormat From { get; } = FrameFormat.Gray2;
+		
 
 		public readonly string Filename;
 		// cROM components
@@ -26,6 +29,7 @@ namespace LibDmd.Converter.cRom
 		private UInt32 NCColors; // Number of colors in palette of colorized ROM=nC
 		private UInt32 NCompMasks; // Number of dynamic masks=nM
 		private UInt32 NMovMasks; // Number of moving rects=nMR
+		private UInt32 NSprites; // Number of sprites=nS
 		private UInt32[] HashCodes; // UINT32[nF] hashcode/checksum
 		private byte[] ShapeCompMode;   // UINT8[nF] FALSE - full comparison (all 4 colors) TRUE - shape mode (we just compare black 0 against all the 3 other colors as if it was 1 color)
 										// HashCode take into account the ShapeCompMode parameter converting any '2' or '3' into a '1'
@@ -37,8 +41,23 @@ namespace LibDmd.Converter.cRom
 		private byte[] CFrames; // UINT8[nF*fW*fH] Colorized frames color indices
 		private byte[] DynaMasks;   // UINT8[nF*fW*fH] Mask for dynamic content for each frame.  The value (<MAX_DYNA_4COLS_PER_FRAME) points to a sequence of 4 colors in Dyna4Cols. 255 means not a dynamic content.
 		private byte[] Dyna4Cols;  // UINT8[nF*MAX_DYNA_4COLS_PER_FRAME*nO] Color sets used to fill the dynamic content
+		private byte[] FrameSprites; // UINT8[nF*MAX_SPRITES_PER_FRAME] Sprite numbers to look for in this frame max=MAX_SPRITES_PER_FRAME
+		private byte[] SpriteDescriptionsO; // UINT8[nS*MAX_SPRITE_SIZE*MAX_SPRITE_SIZE] 4-or-16-color sprite original drawing (255 means this is a transparent=ignored pixel) for Comparison step
+		private byte[] SpriteDescriptionsC; // UINT8[nS*MAX_SPRITE_SIZE*MAX_SPRITE_SIZE] 64-color sprite for Colorization step
+		//private UInt32[] SpriteDetectDwords; // UINT32[nS] dword to quickly detect 4 consecutive distinctive pixels inside the original drawing of a sprite for optimized detection
+		//private UInt16[] SpriteDetectDwordPos; // UINT16[nS] offset of the above dword in the sprite description
+		private byte[] ActiveFrames; // UINT8[nF] is the frame active (colorized or duration>16ms) or not
+		private byte[] ColorRotations; // UINT8[nF*3*MAX_COLOR_ROTATIONS] list of color rotation for each frame:
+									   // 1st byte is color # of the first color to rotate / 2nd byte id the number of colors to rotate / 3rd byte is the length in 10ms between each color switch
+		private UInt16[] SpriteDetAreas; // UINT16[nS*4*MAX_SPRITE_DETECT_AREAS] rectangles (left, top, width, height) as areas to detect sprites (left=0xffff -> no zone)
+		private UInt32[] SpriteDetDwords; // UINT32[nS*MAX_SPRITE_DETECT_AREAS] dword to quickly detect 4 consecutive distinctive pixels inside the original drawing of a sprite for optimized detection
+		private UInt16[] SpriteDetDwordPos; // UINT16[nS*MAX_SPRITE_DETECT_AREAS] offset of the above dword in the sprite description
 
-		private const int MAX_DYNA_4COLS_PER_FRAME = 8;
+		private const int MAX_DYNA_4COLS_PER_FRAME = 16; // max number of color sets for dynamic content for each frame
+		private const int MAX_SPRITE_SIZE = 128; // maximum size of the sprites
+		private const int MAX_SPRITES_PER_FRAME = 32; // maximum amount of sprites to look for per frame
+		private const int MAX_COLOR_ROTATIONS = 8; // maximum amount of color rotations per frame
+		private const int MAX_SPRITE_DETECT_AREAS = 4; // maximum number of areas to detect the sprite
 
 		private bool cromloaded = false; // is there any crom loaded?
 		private UInt32 LastFound = 0; // Which frame was found last time we recognized one?
@@ -47,12 +66,48 @@ namespace LibDmd.Converter.cRom
 		public IObservable<Unit> OnResume { get; }
 		public IObservable<Unit> OnPause { get; }
 		protected readonly Subject<ColoredFrame> ColoredGray6AnimationFrames = new Subject<ColoredFrame>();
+		private byte[] PreviousFrame;
+		private Color[] PreviousPalette;
+		private byte[] PreviousRotations;
+		private byte PreviousSprite=255;
+		//private uint PreviousSpritePos=0;
+		private ushort PreviousFrx = 0;
+		private ushort PreviousFry = 0;
+		private ushort PreviousSpx = 0;
+		private ushort PreviousSpy = 0;
+		private ushort PreviousWid = 0;
+		private ushort PreviousHei = 0;
 
-		public cRom(string filename)
+		public bool isRotation = true;
+
+		private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+		public cRom(string altcolorpath,string romname)
 		{
+			try
+			{
+				ZipFile.ExtractToDirectory(Path.Combine(altcolorpath, romname, romname + ".cRZ"), Path.Combine(altcolorpath, romname));
+			}
+			catch (Exception e)
+			{
+				Logger.Warn(e, $"Could not uncompress the cRZ File");
+				cromloaded = false;
+				return;
+			}
+			uint ti;
 			crce = new CRC32encode();
 			// load a crom file
-			var fs = new FileStream(filename, FileMode.Open);
+			FileStream fs;
+			try
+			{
+				fs = new FileStream(Path.Combine(altcolorpath, romname, romname + ".cRom"), FileMode.Open);
+			}
+			catch (Exception e)
+			{
+				Logger.Warn(e, $"Could not open the cRom File");
+				cromloaded = false;
+				return;
+			}
 			var reader = new BinaryReader(fs);
 			rName = new char[64];
 			rName = reader.ReadChars(64);
@@ -61,11 +116,17 @@ namespace LibDmd.Converter.cRom
 			FHeight = reader.ReadUInt32();
 			NFrames = reader.ReadUInt32();
 			NOColors = reader.ReadUInt32();
+			if (NOColors == 16) From = FrameFormat.Gray4; else From = FrameFormat.Gray2;
 			NCColors = reader.ReadUInt32();
 			NCompMasks = reader.ReadUInt32();
 			NMovMasks = reader.ReadUInt32();
+			if (sizeheader >= 8 * sizeof(UInt32))
+			{
+				NSprites = reader.ReadUInt32();
+			}
+			else NSprites = 0;
 			HashCodes = new UInt32[NFrames];
-			for (uint ti = 0; ti < NFrames; ti++)
+			for (ti = 0; ti < NFrames; ti++)
 				HashCodes[ti] = reader.ReadUInt32();
 			ShapeCompMode = new byte[NFrames];
 			ShapeCompMode = reader.ReadBytes((int)NFrames);
@@ -91,7 +152,39 @@ namespace LibDmd.Converter.cRom
 			DynaMasks = reader.ReadBytes((int)(NFrames * FHeight * FWidth));
 			Dyna4Cols = new byte[NFrames * MAX_DYNA_4COLS_PER_FRAME * NOColors];
 			Dyna4Cols = reader.ReadBytes((int)(NFrames * MAX_DYNA_4COLS_PER_FRAME * NOColors));
+			FrameSprites = new byte[NFrames * MAX_SPRITES_PER_FRAME];
+			FrameSprites = reader.ReadBytes((int)(NFrames * MAX_SPRITES_PER_FRAME));
+			SpriteDescriptionsO = new byte[NSprites * MAX_SPRITE_SIZE * MAX_SPRITE_SIZE];
+			SpriteDescriptionsC = new byte[NSprites * MAX_SPRITE_SIZE * MAX_SPRITE_SIZE];
+			for (ti = 0; ti < NSprites * MAX_SPRITE_SIZE * MAX_SPRITE_SIZE; ti++)
+			{
+				SpriteDescriptionsC[ti] = reader.ReadByte();
+				SpriteDescriptionsO[ti] = reader.ReadByte();
+			}
+			/*SpriteDetectDwords = new UInt32[NSprites];
+			for (ti = 0; ti < NSprites; ti++)
+				SpriteDetectDwords[ti] = reader.ReadUInt32();
+			SpriteDetectDwordPos = new UInt16[NSprites];
+			for (ti = 0; ti < NSprites; ti++)
+				SpriteDetectDwordPos[ti] = reader.ReadUInt16();*/
+			ActiveFrames=new byte[NFrames];
+			ActiveFrames = reader.ReadBytes((int)NFrames);
+			ColorRotations=new byte[NFrames*3*MAX_COLOR_ROTATIONS];
+			ColorRotations = reader.ReadBytes((int)NFrames * 3 * MAX_COLOR_ROTATIONS);
+			SpriteDetDwords = new UInt32[NSprites * MAX_SPRITE_DETECT_AREAS];
+			for (ti = 0; ti < NSprites * MAX_SPRITE_DETECT_AREAS; ti++)
+				SpriteDetDwords[ti] = reader.ReadUInt32();
+			SpriteDetDwordPos = new UInt16[NSprites * MAX_SPRITE_DETECT_AREAS];
+			for (ti = 0; ti < NSprites * MAX_SPRITE_DETECT_AREAS; ti++)
+				SpriteDetDwordPos[ti] = reader.ReadUInt16();
+			SpriteDetAreas = new UInt16[NSprites * 4 * MAX_SPRITE_DETECT_AREAS];
+			for (ti = 0; ti < NSprites * 4 * MAX_SPRITE_DETECT_AREAS; ti++)
+				SpriteDetAreas[ti] = reader.ReadUInt16();
 			reader.Close();
+			PreviousFrame = new byte[FWidth * FHeight];
+			PreviousPalette = new Color[64];
+			PreviousRotations = new byte[MAX_COLOR_ROTATIONS * 3];
+			File.Delete(Path.Combine(altcolorpath, romname, romname + ".cRom"));
 			cromloaded = true;
 		}
 
@@ -107,7 +200,18 @@ namespace LibDmd.Converter.cRom
 			CFrames = null;
 			DynaMasks = null;
 			Dyna4Cols = null;
+			FrameSprites= null;
+			SpriteDescriptionsC = null;
+			SpriteDescriptionsO= null;
+			SpriteDetDwords = null;
+			SpriteDetDwordPos = null;
+			SpriteDetAreas = null;
+			ActiveFrames = null;
+			ColorRotations= null;
 			crce = null;
+			PreviousFrame = null;
+			PreviousPalette = null;
+			PreviousRotations= null;
 			cromloaded = false;
 		}
 
@@ -166,6 +270,7 @@ namespace LibDmd.Converter.cRom
 					}
 				}
 				tj++;
+				if (tj == NFrames) tj = 0;
 				while ((tj != LastFound) && (framechecked[tj] == true))
 				{
 					tj++;
@@ -174,17 +279,122 @@ namespace LibDmd.Converter.cRom
 			} while (tj != LastFound);
 			return -1;
 		}
-
+		private void Check_Sprites(byte[] Frame, Int32 quelleframe, ref byte quelsprite, ref ushort frx,ref ushort fry, ref ushort spx, ref ushort spy, ref ushort wid,ref ushort hei)
+		{
+			byte ti=0;
+			UInt32 mdword;
+			while ((ti < MAX_SPRITES_PER_FRAME) && (FrameSprites[quelleframe * MAX_SPRITES_PER_FRAME + ti] < 255)) 
+			{
+				byte qspr = FrameSprites[quelleframe * MAX_SPRITES_PER_FRAME + ti];
+				for (UInt32 tm=0;tm<MAX_SPRITE_DETECT_AREAS;tm++)
+				{
+					if (SpriteDetAreas[qspr * MAX_SPRITE_DETECT_AREAS * 4 + tm * 4] == 0xffff) continue;
+					// we look for the sprite in the frame sent
+					mdword = (UInt32)(Frame[0] << 8) | (UInt32)(Frame[1] << 16) | (UInt32)(Frame[2] << 24);
+					for (UInt16 tj = 0; tj < FWidth * FHeight - 3; tj++)
+					{
+						mdword = (mdword >> 8) | (UInt32)(Frame[tj + 3] << 24);
+						// we look for the magic dword first
+						UInt16 sddp = SpriteDetDwordPos[qspr * MAX_SPRITE_DETECT_AREAS + tm];
+						if (mdword == SpriteDetDwords[qspr * MAX_SPRITE_DETECT_AREAS + tm]) 
+						{
+							short frax = (short)(tj % FWidth);
+							short fray = (short)(tj / FWidth);
+							short sprx = (short)(sddp % MAX_SPRITE_SIZE);
+							short spry = (short)(sddp / MAX_SPRITE_SIZE);
+							short detx = (short)SpriteDetAreas[qspr * MAX_SPRITE_DETECT_AREAS * 4 + tm * 4];
+							short dety = (short)SpriteDetAreas[qspr * MAX_SPRITE_DETECT_AREAS * 4 + tm * 4 + 1];
+							short detw = (short)SpriteDetAreas[qspr * MAX_SPRITE_DETECT_AREAS * 4 + tm * 4 + 2];
+							short deth = (short)SpriteDetAreas[qspr * MAX_SPRITE_DETECT_AREAS * 4 + tm * 4 + 3];
+							if ((frax < sprx - detx) || (fray < spry - dety)) continue; // if the detection area is outside the frame, continue
+							int offsx = frax - sprx + detx;
+							int offsy = fray - spry + dety;
+							if ((offsx + detw >= FWidth) || (offsy + deth >= FHeight)) continue;
+							// we can now check if the sprite is there
+							bool notthere = false;
+							for (UInt16 tk = 0; tk < deth; tk++) 
+							{
+								for (UInt16 tl = 0; tl < detw; tl++)
+								{
+									byte val = SpriteDescriptionsO[qspr * MAX_SPRITE_SIZE * MAX_SPRITE_SIZE + (tk + dety) * MAX_SPRITE_SIZE + tl + detx];
+									if (val == 255) continue;
+									if (val != Frame[(tk + offsy) * FWidth + tl + offsx])
+									{
+										notthere = true;
+										break;
+									}
+								}
+								if (notthere == true) break;
+							}
+							if (!notthere)
+							{
+								quelsprite = qspr;
+								/*								frx = (ushort)Math.Max(0, frax - sprx);
+																fry = (ushort)Math.Max(0, fray - spry);
+																spx = (ushort)Math.Max(0, sprx - frax);
+																spy = (ushort)Math.Max(0, spry - fray);
+																wid = (ushort)Math.Min(Math.Min((int)FWidth - (frax - sprx), (int)FWidth), MAX_SPRITE_SIZE);
+																hei = (ushort)Math.Min(Math.Min((int)FHeight - (fray - spry), (int)FHeight), MAX_SPRITE_SIZE);
+								*/
+								if (frax < sprx)
+								{
+									spx = (ushort)(sprx - frax);
+									frx = 0;
+									wid = Math.Min((ushort)FWidth, (ushort)(MAX_SPRITE_SIZE - spx));
+								}
+								else
+								{
+									spx = 0;
+									frx = (ushort)(frax - sprx);
+									wid = Math.Min((ushort)(FWidth - frx), (ushort)(MAX_SPRITE_SIZE - frx));
+								}
+								if (fray < spry)
+								{
+									spy = (ushort)(spry - fray);
+									fry = 0;
+									hei = Math.Min((ushort)FHeight, (ushort)(MAX_SPRITE_SIZE - spy));
+								}
+								else
+								{
+									spy = 0;
+									fry = (ushort)(fray - spry);
+									hei = Math.Min((ushort)(FHeight - fry), (ushort)(MAX_SPRITE_SIZE - fry));
+								}
+								return;
+							}
+						}
+					}
+				}
+				ti++;
+			}
+			quelsprite = 255;
+			return;
+		}
 		private void Colorize_Frame(byte[] frame,Int32 IDfound)
 		{
+			UInt32 ti;
 			// Generate the colorized version of a frame once identified in the crom frames
-			for (UInt32 ti = 0; ti < FWidth * FHeight; ti++) 
+			for (ti = 0; ti < FWidth * FHeight; ti++) 
 			{
 				byte dynacouche = DynaMasks[IDfound * FWidth * FHeight + ti];
 				if (dynacouche == 255)
 					frame[ti] = CFrames[IDfound * FWidth * FHeight + ti];
 				else
 					frame[ti] = Dyna4Cols[IDfound * MAX_DYNA_4COLS_PER_FRAME * NOColors + dynacouche * NOColors + frame[ti]];
+			}
+		}
+
+		private void Colorize_Sprite(byte[] frame,byte nosprite,ushort frx,ushort fry,ushort spx,ushort spy,ushort wid,ushort hei)
+		{
+			for (uint tj=0;tj<hei;tj++)
+			{
+				for (uint ti = 0; ti < wid; ti++)
+				{
+					if (SpriteDescriptionsO[(nosprite * MAX_SPRITE_SIZE + tj + spy) * MAX_SPRITE_SIZE + ti + spx] < 255)
+					{
+						frame[(fry + tj) * FWidth + frx + ti] = SpriteDescriptionsC[(nosprite * MAX_SPRITE_SIZE + tj + spy) * MAX_SPRITE_SIZE + ti + spx];
+					}
+				}
 			}
 		}
 
@@ -230,16 +440,18 @@ namespace LibDmd.Converter.cRom
 			byte[][] planes = new byte[6][];
 			for (int i = 0; i < 6; i++) planes[i] = new byte[FWidth * FHeight / 8];
 			Color[] palette = new Color[64];
+			byte[] rotations = new byte[MAX_COLOR_ROTATIONS * 3];
 			byte[] Frame=new byte[FWidth*FHeight];
 			for (uint ti = 0; ti < FWidth * FHeight; ti++) Frame[ti] = frame.Data[ti];
-			/*if (Dimensions.Value.Width * Dimensions.Value.Height != frame.Data.Length * 4)
-				planes = FrameUtil.Split(Dimensions.Value.Width, Dimensions.Value.Height, 2, frame.Data);
-			else
-				planes = FrameUtil.Split(Dimensions.Value.Width / 2, Dimensions.Value.Height / 2, 2, frame.Data);*/
+			// We check if we find a sprite in the frame
 			// Let's first identify the incoming frame among the ones we have in the crom
 			Int32 IDfound = Identify_Frame(Frame);
-			if (IDfound == -1)
+			byte nosprite = 255;
+			uint possprite = 0;
+			ushort frx = 0, fry = 0, spx = 0, spy = 0, wid = 0, hei = 0;
+			if ((IDfound == -1) || (ActiveFrames[IDfound] == 0))
 			{
+				/*// code for the colorization team
 				for (uint ti=0;ti<NOColors;ti++)
 				{
 					palette[ti].A = 255;
@@ -257,16 +469,44 @@ namespace LibDmd.Converter.cRom
 					{
 						Frame[ti + tj * FWidth] = 16;
 					}
-				}
-				// Let's now generate the corresponding colorized frame
+				}*/
+				// code for the players
+				for (uint ti = 0; ti < FWidth * FHeight; ti++) Frame[ti] = PreviousFrame[ti];
+				for (uint ti = 0; ti < 64; ti++) palette[ti] = PreviousPalette[ti];
+				for (uint ti = 0; ti < 3 * MAX_COLOR_ROTATIONS; ti++) rotations[ti] = PreviousRotations[ti];
+				nosprite = PreviousSprite;
+				frx = PreviousFrx;
+				fry = PreviousFry;
+				spx = PreviousSpx;
+				spy = PreviousSpy;
+				wid = PreviousWid;
+				hei = PreviousHei;
 			}
 			else
 			{
+				Check_Sprites(Frame, IDfound, ref nosprite,ref frx,ref fry,ref spx,ref spy,ref wid,ref hei);
 				Colorize_Frame(Frame, IDfound);
 				Copy_Frame_Palette(IDfound, palette);
+				if (nosprite < 255)
+				{
+					Colorize_Sprite(Frame, nosprite, frx, fry, spx, spy, wid, hei);
+				}
+				for (uint ti = 0; ti < FWidth * FHeight; ti++) PreviousFrame[ti] = Frame[ti];
+				for (uint ti = 0; ti < 64; ti++) PreviousPalette[ti] = palette[ti];
+				for (uint ti = 0; ti < MAX_COLOR_ROTATIONS * 3; ti++)
+				{
+					PreviousRotations[ti] = rotations[ti] = ColorRotations[IDfound * 3 * MAX_COLOR_ROTATIONS + ti];
+				}
+				PreviousSprite = nosprite;
+				PreviousFrx= frx;
+				PreviousFry= fry;
+				PreviousSpx= spx;
+				PreviousSpy= spy;
+				PreviousWid= wid;
+				PreviousHei= hei;
 			}
 			Copy_Frame_to_Planes(Frame, planes, 6);
-			ColoredGray6AnimationFrames.OnNext(new ColoredFrame(planes, palette));
+			ColoredGray6AnimationFrames.OnNext(new ColoredFrame(planes, palette, rotations));
 		}
 		public void Convert(DMDFrame frame)
 		{
