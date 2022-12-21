066a287a0f07f944027e7eb9188599c9cac29617
David <okidoaki@hotmail.fr>
2022-12-21 12:10:31 +0100

    UInt32 -> uint

--- LibDmd/Converter/Serum/CRC32encode.cs
+++ LibDmd/Converter/Serum/CRC32encode.cs
@@ -5,18 +5,18 @@ namespace LibDmd.Converter.Serum
 	public class CRC32encode
 	{
 		private bool crc32_ready = false;
-		private UInt32[] crc32_table=new UInt32[256];
+		private uint[] crc32_table=new uint[256];
 
 		public CRC32encode() // initiating the CRC table, must be called at startup
 		{
 			if (crc32_ready) return;
 			for (uint i = 0; i < 256; i++)
 			{
-				UInt32 ch = i;
-				UInt32 crc = 0;
+				uint ch = i;
+				uint crc = 0;
 				for (uint j = 0; j < 8; j++)
 				{
-					UInt32 b = (ch ^ crc) & 1;
+					uint b = (ch ^ crc) & 1;
 					crc >>= 1;
 					if (b!=0) crc = crc ^ 0xEDB88320;
 					ch >>= 1;
@@ -26,10 +26,10 @@ namespace LibDmd.Converter.Serum
 			crc32_ready = true;
 		}
 
-		public UInt32 crc32_fast(byte[] s, uint n, byte ShapeMode) // computing a buffer CRC32, "init_crc32()" must have been called before the first use
+		public uint crc32_fast(byte[] s, uint n, byte ShapeMode) // computing a buffer CRC32, "init_crc32()" must have been called before the first use
 		{
 
-			UInt32 crc = 0xFFFFFFFF;
+			uint crc = 0xFFFFFFFF;
 			for (uint i = 0; i<n; i++)
 			{
 				byte val = s[i];
@@ -39,10 +39,10 @@ namespace LibDmd.Converter.Serum
 			return ~crc;
 		}
 
-		public UInt32 crc32_fast_mask(byte[] source, byte[] mask, uint n, byte ShapeMode) // computing a buffer CRC32 on the non-masked area, "init_crc32()" must have been called before the first use
+		public uint crc32_fast_mask(byte[] source, byte[] mask, uint n, byte ShapeMode) // computing a buffer CRC32 on the non-masked area, "init_crc32()" must have been called before the first use
 																								   // take into account if we are in shape mode
 		{
-			UInt32 crc = 0xFFFFFFFF;
+			uint crc = 0xFFFFFFFF;
 			for (uint i = 0; i < n; i++)
 			{
 				if (mask[i] == 0)
