a6a25bbbecae2ecfeed95ecf05e0ac4bf0aba35e
freezy <freezy@kodi.tv>
2023-03-05 14:10:05 +0100

    project: Remove reference to .NET 4.5.2.
    (cherry picked from commit 298e3e388442f8d643c3780d7404129412266d3b)

--- Console/DmdExt.cs
+++ Console/DmdExt.cs
@@ -249,7 +249,7 @@ namespace DmdExt
 			using (var ndpKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry32).OpenSubKey(@"SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full\")) {
 				var releaseKey = Convert.ToInt32(ndpKey?.GetValue("Release"));
 				if (releaseKey < 379893) {
-					System.Console.WriteLine("You need to install at least v4.5.2 of the .NET framework.");
+					System.Console.WriteLine("You need to install at least v4.7.2 of the .NET framework.");
 					System.Console.WriteLine("Download from here: https://www.microsoft.com/en-us/download/details.aspx?id=42642");
 					Environment.Exit(CommandLine.Parser.DefaultExitCodeFail);
 				}
