0020d3c3afa885a358687df02f81c259800d1d21
Joe Flint <gyrojoe@gmail.com>
2023-04-16 14:31:12 -0700

    Fixes typo in name

---
+++ LibDmd/Input/PinballFX/PinballFX3MemoryGrabberBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace LibDmd.Input.PinballFX
+{
+	public abstract class PinballFX3MemoryGrabberBase<FrameType>: MemoryGrabber<FrameType>
+	{
+		protected override IntPtr AttachGameProcess(Process p)
+		{
+			if (p.ProcessName == "Pinball FX3")
+			{
+				return GetPointerBaseAddress(p);
+			}
+			return IntPtr.Zero;
+		}
+
+		abstract protected IntPtr GetPointerBaseAddress(Process gameProc);
+	}
+}
