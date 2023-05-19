920b47610bc219bcee9867121ba6c179158460e7
freezy <freezy@kodi.tv>
2023-05-19 23:04:19 +0200

    release: v2.1.2.

--- VersionAssemblyInfo.cs
+++ VersionAssemblyInfo.cs
@@ -19,5 +19,5 @@ using System.Reflection;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.1.2.0")]
 [assembly: AssemblyFileVersion("2.1.2.0")]
-[assembly: AssemblyInformationalVersion("2.1.2-SNAPSHOT")]
+[assembly: AssemblyInformationalVersion("2.1.2")]
 [assembly: NeutralResourcesLanguage("en")]
