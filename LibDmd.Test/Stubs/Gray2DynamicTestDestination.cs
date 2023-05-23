540c563196e2b239997bc7e3fa1b73b935b49a11
freezy <freezy@kodi.tv>
2023-05-23 23:43:05 +0200

    test: Add coverage for dynamic size and flipping.

---
+++ LibDmd.Test/Stubs/Gray2DynamicTestDestination.cs
@@ -0,0 +1,20 @@
+﻿using LibDmd.Frame;
+using LibDmd.Output;
+
+namespace LibDmd.Test.Stubs
+{
+	public class Gray2DynamicTestDestination : DynamicTestDestination<DmdFrame>, IGray2Destination
+	{
+		public string Name => "Test Destination (Dynamic Gray2)";
+		public bool IsAvailable => true;
+
+		public void RenderGray2(DmdFrame frame)
+		{
+			LastFrame.OnNext(frame);
+		}
+		
+		public void Dispose()
+		{
+		}
+	}
+}
---
+++ LibDmd.Test/Stubs/Rgb24FixedTestDestination.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows.Media;
+using LibDmd.Frame;
+using LibDmd.Output;
+
+namespace LibDmd.Test.Stubs
+{
+	public class Rgb24FixedTestDestination : FixedTestDestination<DmdFrame>, IRgb24Destination
+	{
+		public string Name => "Test Destination (Fixed RGB24)";
+		public bool IsAvailable => true;
+
+		public Rgb24FixedTestDestination(int dmdWidth, int dmdHeight, bool dmdAllowHdScaling = true) : base(dmdWidth, dmdHeight, dmdAllowHdScaling)
+		{
+		}
+
+		public void RenderRgb24(DmdFrame frame)
+		{
+			LastFrame.OnNext(frame);
+		}
+		
+		public void Dispose()
+		{
+		}
+
+		public void ClearPalette()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void ClearColor()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void SetPalette(Color[] colors, int index = -1)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void SetColor(Color color)
+		{
+			throw new NotImplementedException();
+		}
+
+	}
+}
