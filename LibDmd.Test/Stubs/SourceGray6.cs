df2ce83ad3bf7b675ffb8e4a4a79115349091b97
freezy <freezy@kodi.tv>
2023-05-26 22:13:10 +0200

    refactor: gray6 -> *

---
+++ LibDmd.Test/Stubs/SourceGray6.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reactive;
+using System.Reactive.Subjects;
+using LibDmd.Frame;
+using LibDmd.Input;
+
+namespace LibDmd.Test.Stubs
+{
+	public class SourceGray6 : IGray6Source, ITestSource<DmdFrame>
+	{
+		public string Name => "Gray6 Test Source";
+
+		public BehaviorSubject<Dimensions> Dimensions { get; set; } = new BehaviorSubject<Dimensions>(new Dimensions(128, 32));
+
+		public IObservable<Unit> OnResume => null;
+		public IObservable<Unit> OnPause => null;
+
+		private readonly Subject<DmdFrame> _frames = new Subject<DmdFrame>();
+
+		public IObservable<DmdFrame> GetGray6Frames() => _frames;
+
+		public void AddFrame(DmdFrame frame)
+		{
+			Dimensions.OnNext(frame.Dimensions);
+			_frames.OnNext(frame);
+		}
+	}
+}
