7e52dfb209f9f474d132a366063afe1302d36b08
freezy <freezy@kodi.tv>
2023-06-25 22:27:03 +0200

    refactor: Handle extra planes differently.

--- LibDmd/Frame/RawFrame.cs
+++ LibDmd/Frame/RawFrame.cs
@@ -3,11 +3,16 @@
 	public class RawFrame : DmdFrame
 	{
 		public byte[][] RawPlanes;
+		public byte[][] ExtraRawPlanes;
 
-		public RawFrame Update(Dimensions dim, byte[] data, byte[][]rawPlanes)
+		public int TotalPlanes => RawPlanes.Length + ExtraRawPlanes.Length;
+		public int PlaneSize => RawPlanes.Length > 0 ? RawPlanes[0].Length : 0;
+
+		public RawFrame Update(Dimensions dim, byte[] data, byte[][]rawPlanes, byte[][] extraRawPlanes)
 		{
 			Update(dim, data, rawPlanes.Length);
 			RawPlanes = rawPlanes;
+			ExtraRawPlanes = extraRawPlanes;
 			return this;
 		}
 	}
