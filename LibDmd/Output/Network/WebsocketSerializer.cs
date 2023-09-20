aa022b9b01edf9234efc6f4abdec716f26f158f5
Westworld <Thomas.Maul@gmail.com>
2023-09-20 20:47:04 +0200

    color6: add color rotation to JavaScript

--- LibDmd/Output/Network/WebsocketSerializer.cs
+++ LibDmd/Output/Network/WebsocketSerializer.cs
@@ -187,7 +187,7 @@ namespace LibDmd.Output.Network
 				}
 			}
 			else
-				for (var i = 0; i< 8; i++) {
+				for (var i = 0; i< 24; i++) {
 					buffer[i] = rotations[i];
 				}
 			var data = Encoding.ASCII
