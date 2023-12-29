93d3014cf6f647dcf25115c5803fbf8202ad91d1
Markus Kalkbrenner <git@kalki.de>
2023-12-29 20:01:53 +0100

    fixed dimensions and scaling

--- LibDmd/Output/ZeDMD/ZeDMDWiFi.cs
+++ LibDmd/Output/ZeDMD/ZeDMDWiFi.cs
@@ -9,21 +9,25 @@ namespace LibDmd.Output.ZeDMD
 	/// Check "ZeDMD Project Page" https://github.com/PPUC/ZeDMD) for details.
 	/// This implementation supports ZeDMD and ZeDMD HD.
 	/// </summary>
-	public class ZeDMDWiFi : ZeDMDBase, IGray2Destination, IGray4Destination, IColoredGray2Destination, IColoredGray4Destination, IColoredGray6Destination, IFixedSizeDestination, IColorRotationDestination
+	public class ZeDMDWiFi : ZeDMDBase, IGray2Destination, IGray4Destination, IColoredGray2Destination, IColoredGray4Destination, IColoredGray6Destination, IMultiSizeDestination, IColorRotationDestination
 	{
 		public override string Name => "ZeDMD WiFi";
 		public string WifiAddress { get; set; }
 		public int WifiPort { get; set; }
 		public string WifiSsid { get; set; }
 		public string WifiPassword { get; set; }
-		// In WiFi mode we can't leverage ZeDMD's own advanced downscaling.
-		// Implementing IFixedSize activates DMDExt's downsacling.
+		// To leverage ZeDMD's own advanced downscaling we can't use FixedSize and RGB24Stream like ZeDMD HD.
+		// By not declaring 192x62 supported, we get a centered 256x64 frame.
+		public Dimensions[] Sizes { get; } = { new Dimensions(128, 16), Dimensions.Standard, new Dimensions(256, 64) };
+		// FixedSize is just needed for inheritance.
 		public override Dimensions FixedSize { get; } = Dimensions.Standard;
+		// DmdAllowHdScaling is just needed for inheritance.
 		public override bool DmdAllowHdScaling { get; set; } = false;
 		// libzedmd has it's own queuing.
 		public override int Delay { get; set; } = 0;
 
 		private static ZeDMDWiFi _instance;
+		protected Dimensions _currentDimensions = Dimensions.Standard;
 
 		/// <summary>
 		/// Returns the current instance of ZeDMD.
@@ -83,42 +87,58 @@ namespace LibDmd.Output.ZeDMD
 			Logger.Info(Name + " device found");
 
 			if (Debug) { ZeDMD_EnableDebug(_pZeDMD); }
-			ZeDMD_SetFrameSize(_pZeDMD, FixedSize.Width, FixedSize.Height);
+			ZeDMD_SetFrameSize(_pZeDMD, _currentDimensions.Width, _currentDimensions.Height);
+			ZeDMD_EnablePreDownscaling(_pZeDMD);
+			ZeDMD_EnablePreUpscaling(_pZeDMD);
 			if (Brightness >= 0 && Brightness <= 15) { ZeDMD_SetBrightness(_pZeDMD, Brightness); }
 			if (RgbOrder >= 0 && RgbOrder <= 5) { ZeDMD_SetRGBOrder(_pZeDMD, RgbOrder); }
 		}
 
+		public void SetDimensions(Dimensions newDim)
+		{
+			if (_currentDimensions != newDim) {
+				_currentDimensions = newDim;
+				ZeDMD_SetFrameSize(_pZeDMD, newDim.Width, newDim.Height);
+			}
+		}
+
 		public void RenderGray2(DmdFrame frame)
 		{
+			SetDimensions(frame.Dimensions);
 			ZeDMD_RenderGray2(_pZeDMD, frame.Data);
 		}
 
 		public void RenderColoredGray2(ColoredFrame frame)
 		{
+			SetDimensions(frame.Dimensions);
 			SetPalette(frame.Palette);
 			ZeDMD_RenderGray2(_pZeDMD, frame.Data);
 		}
 
 		public void RenderGray4(DmdFrame frame)
 		{
+			SetDimensions(frame.Dimensions);
 			ZeDMD_RenderGray4(_pZeDMD, frame.Data);
 		}
 
 		public void RenderColoredGray4(ColoredFrame frame)
 		{
+			SetDimensions(frame.Dimensions);
 			SetPalette(frame.Palette);
 			ZeDMD_RenderGray4(_pZeDMD, frame.Data);
 		}
 
 		public void RenderColoredGray6(ColoredFrame frame)
 		{
-			_lastFrame = (ColoredFrame)frame.Clone();
+			SetDimensions(frame.Dimensions);
 			SetPalette(frame.Palette);
 			ZeDMD_RenderColoredGray6(_pZeDMD, frame.Data, frame.Rotations);
+			_lastFrame = (ColoredFrame)frame.Clone();
 		}
 
 		public void RenderRgb24(DmdFrame frame)
 		{
+			SetDimensions(frame.Dimensions);
 			ZeDMD_RenderRgb24(_pZeDMD, frame.Data);
 		}
 
