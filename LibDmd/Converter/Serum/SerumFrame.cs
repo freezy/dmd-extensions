18c493e33034b9e33ce1b6256dfef149cdeab3c5
freezy <freezy@kodi.tv>
2024-05-29 00:16:05 +0200

    serum: Update API to v2.

---
+++ LibDmd/Converter/Serum/SerumFrame.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace LibDmd.Converter.Serum
+{
+	public struct SerumFrame
+	{
+		// data for v1 Serum format
+		public IntPtr frame; // return the colorized frame
+		public IntPtr palette; // and its palette
+		public IntPtr rotations; // and its color rotations
+		// data for v2 Serum format
+		// the frame (frame32 or frame64) corresponding to the resolution of the ROM must ALWAYS be defined
+		// if a frame pointer is defined, its width, rotations and rotationsinframe pointers must be defined
+		public IntPtr frame32;
+		public uint width32; // 0 is returned if the 32p colorized frame is not available for this frame
+		public IntPtr rotations32;
+		public IntPtr rotationsinframe32; // [width32*32*2] precalculated array to tell if a color is in a color rotations of the frame ([X*Y*0]=0xffff if not part of a rotation)
+		public IntPtr modifiedelements32; // (optional) 32P pixels modified during the last rotation
+		public IntPtr frame64;
+		public uint width64; // 0 is returned if the 64p colorized frame is not available for this frame
+		public IntPtr rotations64;
+		public IntPtr rotationsinframe64;  // [width64*64*2] precalculated array to tell if a color is in a color rotations of the frame ([X*Y*0]=0xffff if not part of a rotation)
+		public IntPtr modifiedelements64; // (optional) 64P pixels modified during the last rotation
+		// common data
+		public uint SerumVersion; // SERUM_V1 or SERUM_V2
+		/// <summary>
+		/// flags for return:
+		/// if flags & 1 : frame32 has been filled
+		/// if flags & 2 : frame64 has been filled
+		/// if flags & 4 : frame + palette have been filled
+		/// if none of them, display the original frame
+		/// </summary>
+		public byte flags;
+		public uint nocolors; // number of shades of orange in the ROM
+		public uint ntriggers; // number of triggers in the Serum file
+		public uint triggerID; // return 0xffff if no trigger for that frame, the ID of the trigger if one is set for that frame
+		public uint frameID; // for CDMD ingame tester
+		public ushort rotationtimer;
+	}
+}
---
+++ LibDmd/Converter/Serum/SerumVersion.cs
@@ -0,0 +1,12 @@
+﻿// ReSharper disable InconsistentNaming
+namespace LibDmd.Converter.Serum
+{
+	/// <summary>
+	/// Returned by Serum_Load in *SerumVersion
+	/// </summary>
+	enum SerumVersion
+	{
+		Version1,
+		Version2
+	}
+}
