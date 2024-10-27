50fb11ee17f8340624bd914c002d896e84f8e2dd
freezy <freezy@kodi.tv>
2024-10-27 14:15:44 +0100

    serum: Add missing RGB565 implementation.

--- LibDmd/Output/ZeDMD/ZeDMDWiFi.cs
+++ LibDmd/Output/ZeDMD/ZeDMDWiFi.cs
@@ -7,7 +7,8 @@ namespace LibDmd.Output.ZeDMD
 	/// Check "ZeDMD Project Page" https://github.com/PPUC/ZeDMD) for details.
 	/// This implementation supports ZeDMD and ZeDMD HD.
 	/// </summary>
-	public class ZeDMDWiFi : ZeDMDWiFiBase, IGray2Destination, IGray4Destination, IColoredGray2Destination, IColoredGray4Destination, IColoredGray6Destination, IMultiSizeDestination, IColorRotationDestination
+	public class ZeDMDWiFi : ZeDMDWiFiBase, IGray2Destination, IGray4Destination, IColoredGray2Destination,
+		IColoredGray4Destination, IRgb565Destination, IColoredGray6Destination, IMultiSizeDestination, IColorRotationDestination
 	{
 		public override string Name => "ZeDMD WiFi";
 		// To leverage ZeDMD's own advanced downscaling we can't use FixedSize and RGB24Stream like ZeDMD HD.
@@ -86,6 +87,12 @@ namespace LibDmd.Output.ZeDMD
 		{
 			SetDimensions(frame.Dimensions);
 			ZeDMD_RenderRgb24(_pZeDMD, frame.Data);
+		}
+
+		public void RenderRgb565(DmdFrame frame)
+		{
+			SetDimensions(frame.Dimensions);
+			ZeDMD_RenderRgb16(_pZeDMD, frame.Data);
 		}
 	}
 }
